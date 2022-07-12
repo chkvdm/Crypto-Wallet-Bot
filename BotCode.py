8a142092d0ebdc9cabf7fd4745ec994cd41e664a
chkvdm <vadikvr6@outlook.com>
2022-07-12 15:29:43 +0400

    Try/Except

--- BotCode.py
+++ BotCode.py
@@ -16,40 +16,48 @@ class user(object):
   def buy(self):
     start = True
     while start:
-      amount_btc = (float(input("Enter BTC amount you want buy: ")))
-      if (amount_btc * btc_cost) > self.balance["USD"]:
-        try_again = (input("There are not enough funds in your account. Enter Y fo continue, enter N for exit: "))
-        try_again = try_again.upper()
-        if try_again == "Y":
-          continue
+      try:
+        amount_btc = (float(input("Enter BTC amount you want buy: ")))
+        if (amount_btc * btc_cost) > self.balance["USD"]:
+          try_again = (input("There are not enough funds in your account. Enter Y fo continue, enter N for exit: "))
+          try_again = try_again.upper()
+          if try_again == "Y":
+            continue
+          else:
+            break
         else:
-          break
-      else:
-        self.balance["BTC"] += amount_btc
-        self.balance["BTC"] = round(self.balance["BTC"], 8)
-        self.balance["USD"] -= btc_cost * amount_btc
-        self.balance["USD"] = round(self.balance["USD"], 8)
-        return self.balance
+          self.balance["BTC"] += amount_btc
+          self.balance["BTC"] = round(self.balance["BTC"], 8)
+          self.balance["USD"] -= btc_cost * amount_btc
+          self.balance["USD"] = round(self.balance["USD"], 8)
+          return self.balance
+      except Exception:
+        print ('Incorrect input. Please, enter a valid value.')
   
   def sell(self):
     start = True
     while start:
-      amount_btc = (float(input("Enter BTC amount you want sell: ")))
-      if (amount_btc) > self.balance["BTC"]:
-        try_again = (input("There are not enough funds in your account. Enter Y fo continue, enter N for exit: "))
-        try_again = try_again.upper()
-        if try_again == "Y":
-          continue
+      try:
+        amount_btc = (float(input("Enter BTC amount you want sell: ")))
+        if (amount_btc) > self.balance["BTC"]:
+          try_again = (input("There are not enough funds in your account. Enter Y fo continue, enter N for exit: "))
+          try_again = try_again.upper()
+          if try_again == "Y":
+            continue
+          else:
+            break
         else:
-          break
-      else:
-        self.balance["BTC"] -= amount_btc
-        self.balance["BTC"] = round(self.balance["BTC"], 8)
-        self.balance["USD"] += btc_cost * amount_btc
-        self.balance["USD"] = round(self.balance["USD"], 8)
-        return self.balance
+          self.balance["BTC"] -= amount_btc
+          self.balance["BTC"] = round(self.balance["BTC"], 8)
+          self.balance["USD"] += btc_cost * amount_btc
+          self.balance["USD"] = round(self.balance["USD"], 8)
+          return self.balance
+      except Exception:
+        print ('Incorrect input. Please, enter a valid value.')
   
 vadim = user("Vadim")
 print (vadim)
 vadim.buy()
+print (vadim.get_balance())
+vadim.sell()
 print (vadim.get_balance())
